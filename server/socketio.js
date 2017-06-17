6618c6fb89870c613075837d8f1f5339597c02d1
Thibaud Arnault <thibaud.arnault@gmail.com>
2017-06-17 09:42:11 +0000

    add content

---
+++ server/socketio.js
@@ -0,0 +1,6 @@
+module.exports = function(io) {
+    io.on('connection', (socket) => {
+        //TODO
+        // example :  socket.on('disconnect', () => { console.log("a user disconnect")})
+    })
+}
